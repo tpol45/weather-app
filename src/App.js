f0590c1f2ca17701b38e3544a283115681c6b7c1
Thomas Pollick <thomascpollick@gmail.com>
2019-07-18 14:09:21 -0500

    Added input form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component} from "react";
+import Form from "./Form.js"
 import "./App.css";
 
 class App extends Component{
@@ -6,6 +7,7 @@ class App extends Component{
     return(
       <div className="App">
         <h1> Weather App </h1>
+        <Form />
       </div>
     );
   }
---
+++ src/Form.js
@@ -0,0 +1,46 @@
+import React, {Component} from "react"
+
+class Form extends Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: '',
+            lastName: '',
+            zipCode: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange (evt) {
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+    handleSubmit (evt) {
+        evt.preventDefault()
+        try {
+            this.props.fetchNewCampus(this.state)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    render () {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor='firstName'>First Name</label>
+                <input onChange={this.handleChange} name="firstName" type="text" value={this.state.name} />
+                <label htmlFor='lastName'>Last Name</label>
+                <input onChange={this.handleChange} name="lastName" type="text" value={this.state.address} />
+                <label htmlFor='zipCode'>Zip Code</label>
+                <input onChange={this.handleChange} name="zipCode" type="text" value={this.state.description} />
+                <button type='submit'>Continue</button>
+            </form>
+        )
+    }
+}
+    
+
+export default Form;
