d84f416fdb8baa5044733c716f1cf6c28d6c0738
Thomas Pollick <thomascpollick@gmail.com>
2019-07-20 14:05:55 -0500

    handle invalid zip codes

--- app/components/ResultsPage.js
+++ app/components/ResultsPage.js
@@ -2,16 +2,52 @@ import React, { Component} from "react";
 import LocationInfo from "./LocationInfo"
 import WeatherInfo from "./WeatherInfo"
 import Greeting from "./Greeting"
+import {setMode} from "../redux/mode"
 
 
 export default class ResultsPage extends Component{
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick (evt) {
+    this.props.setEntryMode()
+  }
+
   render(){
-    return(
-      <div>
-        <Greeting />
-        <WeatherInfo />
-        <LocationInfo />
-      </div>
-    );
+    if (!this.props.cityInfo.city){
+      return(
+        <div>
+          <div>City Not Found</div>
+          <button onClick={this.handleClick}>Go Back</button>
+        </div>
+      )
+    }
+    else {
+      return(
+        <div>
+          <Greeting />
+          <WeatherInfo />
+          <LocationInfo />
+        </div>
+      );
+    }
   }
 }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setEntryMode: () => dispatch(setMode('entry'))
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+      cityInfo: state.cityInfo
+    };
+};
+
+export default connect(
+mapStateToProps
+)(ResultsPage)
